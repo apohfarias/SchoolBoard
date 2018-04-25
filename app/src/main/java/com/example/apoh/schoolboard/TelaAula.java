2f900e6d44b1dd5810583fcb9e041361522ff94c
Apoh Farias <apollyane16@gmail.com>
2018-04-25 00:22:37 -0300

    consigo abrir a camera e tirar foto. Falta joga a imagem pra tela.

--- app/src/main/java/com/example/apoh/schoolboard/TelaAula.java
+++ app/src/main/java/com/example/apoh/schoolboard/TelaAula.java
@@ -1,13 +1,51 @@
 package com.example.apoh.schoolboard;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class TelaAula extends AppCompatActivity {
+    Button btnCapturar, btnGaleria;
+    ImageView imageView;
+    private ImageView foto = null;
+
+    final int REQUEST_CODE_GALLERY = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_aula);
+
+        foto = (ImageView)findViewById(R.id.imageView);
+
     }
+
+    public void capturaImagem(View botao) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+    public void onActivityResult(int codigo, int resultado, Intent dados) {
+        super.onActivityResult(codigo, resultado, dados);
+        if (dados != null) {
+            Bundle bundle = dados.getExtras();
+
+            if (bundle != null) {
+                Bitmap imagem = (Bitmap)bundle.get("data");
+                foto.setImageBitmap(imagem);
+
+            }
+        }
+    }
+
+
+
 }
